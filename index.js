3e579d0e938a33eb8ff4f94a4019a737172dc2e0
Greyson Harris <gharris@esri.com>
2019-01-06 21:24:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 /* code goes here */
 document.addEventListener("DOMContentLoaded", e => {
-  document.querySelector("div#hidden-div").addEventListener("click", e => onload('301'));
+  document.querySelector("div#hidden-div").addEventListener("click", e => document.div#hidden-div.onload('301'));
 })
