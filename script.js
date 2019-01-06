9bb85dd7f15858b598ab23dd20494896b79874b2
Greyson Harris <gharris@esri.com>
2019-01-06 21:03:36 +0000

    Automatically backed up by Learn

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.addEventListener("DOMContentLoaded", e => {
-  document.querySelector("body")
-   .addEventListener("click", e => console.log("Reggae, Reggae!"));
-})
+  document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"));
