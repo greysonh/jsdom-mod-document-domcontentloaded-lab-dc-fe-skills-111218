1f7c819659aa80df8d371250083769ad927c193f
Greyson Harris <gharris@esri.com>
2019-01-06 21:35:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 /* code goes here */
 document.addEventListener("DOMContentLoaded", e => {
-  document.querySelector("div#hidden-div").addEventListener("load", e => console.log("it's fucken loaded!"));
+  document.querySelector("div#hidden-div").addEventListener("loaded", e => console.log("it's fucken loaded!"));
 })
