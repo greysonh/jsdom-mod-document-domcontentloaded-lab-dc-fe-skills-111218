cb5c64fde14216768d071224352f6a901965cff9
Greyson Harris <gharris@esri.com>
2019-01-06 21:38:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 /* code goes here */
 document.addEventListener("DOMContentLoaded", e => {
-  document.querySelector("div#hidden-div").addEventListener("loaded", e => document.getElementById));
+  document.querySelector("div#hidden-div").addEventListener("loaded", e => document.getElementById(hidden-div).innerHTML = '301'));
 })
